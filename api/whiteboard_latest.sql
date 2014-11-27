e634eb0b542e7e69a0448daef6024716cead2490
Milstein <milsonmun@yahoo.com>
2014-11-27 12:18:04 -0700

    updated the SQL dump

--- api/whiteboard_latest.sql
+++ api/whiteboard_latest.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Enterprise - MySQL GUI v6.13
-MySQL - 5.6.17 : Database - whiteboard
+MySQL - 5.0.45-community-nt : Database - whiteboard
 *********************************************************************
 */
 
@@ -15,40 +15,19 @@ USE `whiteboard`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-/*Table structure for table `board` */
-
-DROP TABLE IF EXISTS `board`;
-
-CREATE TABLE `board` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `userid` bigint(20) NOT NULL,
-  `content` text NOT NULL,
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Table structure for table `chat` */
-
-DROP TABLE IF EXISTS `chat`;
-
-CREATE TABLE `chat` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 /*Table structure for table `room` */
 
 DROP TABLE IF EXISTS `room`;
 
 CREATE TABLE `room` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `boardid` bigint(20) NOT NULL,
-  `chatid` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL auto_increment,
   `roomname` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  `board_content` text,
+  `chat_content` text,
+  `userid` bigint(20) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `roomname` (`roomname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Table structure for table `user` */
@@ -56,11 +35,11 @@ CREATE TABLE `room` (
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL auto_increment,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `created` datetime NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
